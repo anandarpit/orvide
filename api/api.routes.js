c868dddf24d6176a11464d3caed820e2ed488d22
Arpit Anand <orionarpit@gmail.com>
2021-06-30 17:38:10 +0530

    swagger👍

--- api/api.routes.js
+++ api/api.routes.js
@@ -1,9 +1,9 @@
 const router = require(`express`).Router();
 
-router.use('/register', require('./auth/auth.register'));
-router.use('/login', require('./auth/auth.login'));
-router.use('/verifyEmail', require('./auth/auth.verifyEmail'))
-router.use('/uname_unique', require('./meta/meta.uname_unique'))
+router.use('/auth/register', require('./auth/auth.register'));
+router.use('/auth/login', require('./auth/auth.login'));
+router.use('/auth/verifyEmail', require('./auth/auth.verifyEmail'))
+router.use('/meta/uname_unique', require('./meta/meta.uname_unique'))
 
 router.use('/org', require('./auth/auth.org'));
 router.use('/redirectLogin', require('./auth/auth.redirect'));
